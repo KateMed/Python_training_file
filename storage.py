ac0f43735be45e6cfa509fbb321c3eca58d07e60
KateMed <38585405+KateMed@users.noreply.github.com>
2018-06-10 01:13:22 +0300

    Add files via upload

--- storage.py
+++ storage.py
@@ -1,21 +1,41 @@
-import sys
 import argparse
+import json
+import os
+import tempfile
 
-def decorator():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--key', '--val')
-    namespace = parser.parse_args()
-    a = namespace.key
-    return a
-print('lala')
-print('key: ', a)
-'''''
-def storage():
-    tot=[]
-    while True:
-        value=yield tot
-        if not value: break
-        tot+=value
+parser = argparse.ArgumentParser()
+parser.add_argument('--key')
+parser.add_argument('--val')
+parser.add_argument('-r')
+namespace = parser.parse_args()
+b = namespace.val
+a = namespace.key
+c = namespace.r
 
-test=storage.send()
-'''''
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+with open(storage_path, 'r') as f:
+    if f.read() == '' or c is not None:
+        with open(storage_path, 'w') as f:
+            json.dump(dict(), f)
+    else:
+        f.seek(0)
+        data = json.load(f)
+  #      print(data)
+
+if b is None:
+    if a not in data:
+        print(None)
+    else:
+        print(data[a])
+elif a in data:
+    data[a] += ', ' + (b)
+    json_data = json.dumps(data)
+    with open(storage_path, 'w') as f:
+        f.write(json_data)
+  #  print('d', data)
+else:
+    with open(storage_path, 'w') as f:
+        data[a]=b
+        json.dump(data, f)
+   #     print('d', data)
+
